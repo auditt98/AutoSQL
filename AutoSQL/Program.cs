66f74177a7b9cbaa86a075ccad9a9979dca3fe01
Trinh xu <auditt_98@yahoo.com.vn>
2020-09-03 23:20:50 +0700

    .m

--- AutoSQL/Program.cs
+++ AutoSQL/Program.cs
@@ -9,36 +9,61 @@ namespace AutoSQL
 {
     class Program
     {
-        static void Main(string[] args)
-        {
-            var faker = new Faker("vi");
+        private Faker faker = new Faker("vi");
 
-            #region Name
-            var name = new Name();
+        string[] GenMultipleCMT(int n)
+        {
+            string[] listCMT = new string[] { };
+            for (var i = 0; i < n; i++)
+            {
+                var tempCMT = Guid.NewGuid().ToString().Replace("-", string.Empty);
+                var cmt = Regex.Replace(tempCMT, "[a-zA-Z]", string.Empty).Substring(0, 9);
+                listCMT.Append(cmt);
+            }
+            return listCMT;
+        }
+
+        string GenSingleCMT()
+        {
+            var tempCMT = Guid.NewGuid().ToString().Replace("-", string.Empty);
+            var cmt = Regex.Replace(tempCMT, "[a-zA-Z]", string.Empty).Substring(0, 9);
+            return cmt;
+        }
+
+        (string, string, string) GenSingleName()
+        {
             var firstNameList = Enumerable.Range(1, 1000).Select(_ => faker.Name.FirstName()).ToList();
+            //since faker does not support middle name
             var middleNameList = new List<string>() { "Văn", "Hữu", "Thị", "Việt", "Anh", "Đình", "Quang", "Nam", "Đăng" };
             var lastNameList = Enumerable.Range(1, 1000).Select(_ => faker.Name.LastName()).ToList();
-            for (var i = 0; i < 1000; i++)
+            Random r = new Random();
+            var indFirst = r.Next(firstNameList.Count());
+            var indMiddle = r.Next(middleNameList.Count());
+            var indLast = r.Next(lastNameList.Count());
+            return (firstNameList[indFirst], middleNameList[indMiddle], lastNameList[indLast]);
+        }
+
+        (string, string, string)[] GenMultipleName(int n)
+        {
+            (string, string, string)[] listName = new (string, string, string)[] { };
+            var firstNameList = Enumerable.Range(1, 1000).Select(_ => faker.Name.FirstName()).ToList();
+            //since faker does not support middle name
+            var middleNameList = new List<string>() { "Văn", "Hữu", "Thị", "Việt", "Anh", "Đình", "Quang", "Nam", "Đăng" };
+            var lastNameList = Enumerable.Range(1, 1000).Select(_ => faker.Name.LastName()).ToList();
+            for(var i = 0; i < n; i++)
             {
                 Random r = new Random();
                 var indFirst = r.Next(firstNameList.Count());
                 var indMiddle = r.Next(middleNameList.Count());
                 var indLast = r.Next(lastNameList.Count());
-                Console.WriteLine(firstNameList[indFirst] + " " + middleNameList[indMiddle] + " " + lastNameList[indLast]);
+                listName.Append((firstNameList[indFirst], middleNameList[indMiddle], lastNameList[indLast]));
             }
-            #endregion
-
-            #region CMT
-            for (var i = 0; i < 1000; i++)
-            {
-                var tempCMT = Guid.NewGuid().ToString().Replace("-", string.Empty);
-                //var temp2CMT = Guid.NewGuid().ToString().Replace("-", string.Empty);
-                var cmt = Regex.Replace(tempCMT, "[a-zA-Z]", string.Empty).Substring(0, 9);
-                Console.WriteLine(cmt);
-            }
-            #endregion
+            return listName;
+        }
 
 
+        static void Main(string[] args)
+        {
 
         }
     }
